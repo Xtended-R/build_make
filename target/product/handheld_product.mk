32e15ec024bd56c7b970d8a1a4347907e9d62e10
Joey Huab <joey@evolution-x.org>
2020-10-20 19:33:11 +0900

    build: Rename to SettingsIntelligenceAOSP [1/2]
    * To avoid being overwritten by gapps.

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
     LatinIME \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
-    SettingsIntelligence \
+    SettingsIntelligenceAOSP \
     frameworks-base-overlays
 
 PRODUCT_PACKAGES_DEBUG += \
